d1cbfdcc833101c44e8ea8f042eb271990a23248
JulieVedel <juli76k0@edu.easj.dk>
2021-09-06 12:05:53 +0200

    Working calculator
    Can choose different types of symbols

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,8 +14,41 @@ public class Main {
         System.out.println("Hvilket tal skal jeg lægge til?");
         double y = scanner.nextDouble();
 
-        double sum = x - y;
+        double sum = x + y;
         System.out.println("Summen af disse to tal er: " + sum);
+
+//        System.out.println("Lad os nu trække et tal fra dette. Angiv venlist et tal.");
+//        double z = scanner.nextDouble();
+//
+//        double subsum = sum - z;
+//        System.out.println("Den nye sum er nu: " + subsum);
+
+        System.out.println("Lad os prøve en anden lommeregnerfunktion. Skriv et nyt tal.");
+        double tal = scanner.nextDouble();
+
+        System.out.println("Hvordan vil du regne med det nye tal? Skriv +, -, / eller *");
+        String symbol = scanner.next();
+
+        if (symbol.equals("+"))
+        {
+            System.out.println("Resultatet er:" + (sum + tal));
+        }
+        else if (symbol.equals("-"))
+        {
+            System.out.println("Resultatet er:" + (sum - tal));
+        }
+        else if (symbol.equals("/"))
+        {
+            System.out.println("Resultatet er:" + (sum / tal));
+        }
+        else if (symbol.equals("*"))
+        {
+            System.out.println("Resultatet er:" + (sum * tal));
+        }
+        else
+        {
+            System.out.println("Fejl");
+        }
 
     }
 }
