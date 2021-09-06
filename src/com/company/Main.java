792888729e3dad20fa4c68865d8d939836ffd8c7
JulieVedel <juli76k0@edu.easj.dk>
2021-09-06 11:19:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Velkommen til plus-lommeregneren");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Indtast et tal: ");
+        double x = scanner.nextDouble();
+
+        System.out.println("Hvilket tal skal jeg lægge til?");
+        double y = scanner.nextDouble();
+
+        double sum = x + y;
+        System.out.println("Summen af disse to tal er: " + sum);
+
+    }
+}
